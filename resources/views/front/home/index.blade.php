034fbd70991b9a66adbe822d5bb073610cec9635
Murad <agamedov94@mail.ru>
2025-07-19 23:21:24 +0400

    commit

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -741,7 +741,7 @@
                                     <textarea name="contact-message" id="contact-message" cols="30" rows="10"></textarea>
                                 </div>
                             </div>
-
+                            <div class="g-recaptcha mb-3" data-sitekey="{{ env('RECAPTCHA_SITE_KEY') }}"></div>
                             <div class="col-lg-12">
                                 <button name="submit" type="submit" id="submit" class="rn-btn">
                                     <span>{{__("SEND MESSAGE")}}</span>
