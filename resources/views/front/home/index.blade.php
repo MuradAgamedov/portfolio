d6ef01f787edc16aa25577920f42a2fd80eb1f2c
Murad <agamedov94@mail.ru>
2025-07-20 01:19:15 +0400

    commit

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -381,7 +381,7 @@
     <div class="modal-content">
         <div class="modal-header">
             <h3 class="modal-title"></h3>
-            <button class="modal-close" id="closeModal">
+            <button style="width: 60px; height: 60px; cursor: pointer;" class="modal-close" id="closeModal">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M18 6L6 18M6 6L18 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
