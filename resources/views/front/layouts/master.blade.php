43c08828e811c2f98e45e338c9d1b2e8b833ec22
Murad <agamedov94@mail.ru>
2025-08-03 21:15:36 +0400

    commit

--- resources/views/front/layouts/master.blade.php
+++ resources/views/front/layouts/master.blade.php
@@ -89,7 +89,7 @@
                         </div>
                                             <div class="form-group">
                         <label for="email-message">{{__("Message")}}</label>
-                        <textarea id="email-message" name="message"  rows="4" required></textarea>
+                        <textarea id="email-message" name="message" class="form-control" rows="4" required></textarea>
                     </div>
                     <div class="form-group">
                         <div class="g-recaptcha" data-sitekey="{{ env('RECAPTCHA_SITE_KEY') }}" data-callback="onFloatingRecaptchaSuccess" data-expired-callback="onFloatingRecaptchaExpired"></div>
