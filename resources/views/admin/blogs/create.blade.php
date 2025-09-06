dde07ea78205c39bcda1ba9d8528e120c470ee31
Murad <agamedov94@mail.ru>
2025-09-06 12:27:31 +0400

    commit

--- resources/views/admin/blogs/create.blade.php
+++ resources/views/admin/blogs/create.blade.php
@@ -381,6 +381,74 @@
 <!-- CKEditor -->
 <script src="https://cdn.ckeditor.com/ckeditor5/27.1.0/classic/ckeditor.js"></script>
 <script>
+// Russian character conversion functions
+function convertRussianToLatin(text) {
+    const cyrillicMap = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya',
+        'А': 'a', 'Б': 'b', 'В': 'v', 'Г': 'g', 'Д': 'd', 'Е': 'e', 'Ё': 'yo', 'Ж': 'zh', 'З': 'z', 'И': 'i', 'Й': 'y', 'К': 'k', 'Л': 'l', 'М': 'm', 'Н': 'n', 'О': 'o', 'П': 'p', 'Р': 'r', 'С': 's', 'Т': 't', 'У': 'u', 'Ф': 'f', 'Х': 'h', 'Ц': 'ts', 'Ч': 'ch', 'Ш': 'sh', 'Щ': 'sch', 'Ъ': '', 'Ы': 'y', 'Ь': '', 'Э': 'e', 'Ю': 'yu', 'Я': 'ya'
+    };
+    
+    return text.replace(/[а-яёА-ЯЁ]/g, function(match) {
+        return cyrillicMap[match] || match;
+    });
+}
+
+function convertAzerbaijaniToLatin(text) {
+    const azerbaijaniMap = {
+        'ə': 'e', 'ğ': 'gh', 'ü': 'u', 'ş': 'sh', 'ı': 'i', 'ö': 'o', 'ç': 'ch',
+        'Ə': 'e', 'Ğ': 'gh', 'Ü': 'u', 'Ş': 'sh', 'I': 'i', 'Ö': 'o', 'Ç': 'ch'
+    };
+    
+    return text.replace(/[əğüşıöçƏĞÜŞIÖÇ]/g, function(match) {
+        return azerbaijaniMap[match] || match;
+    });
+}
+
+function convertEnglishToLatin(text) {
+    return text;
+}
+
+function convertToLatin(text) {
+    text = convertAzerbaijaniToLatin(text);
+    text = convertRussianToLatin(text);
+    return text;
+}
+
+function generateSlug(text, lang = 'az') {
+    // Convert to lowercase
+    text = text.toLowerCase();
+    
+    // Handle different languages
+    switch (lang) {
+        case 'ru':
+            return convertRussianToLatin(text);
+        case 'az':
+            return convertAzerbaijaniToLatin(text);
+        case 'en':
+            return convertEnglishToLatin(text);
+        default:
+            return convertToLatin(text);
+    }
+}
+
+function generateSlugFinal(text, lang = 'az') {
+    let slug = generateSlug(text, lang);
+    
+    // Remove special characters except spaces and hyphens
+    slug = slug.replace(/[^\w\s-]/g, '');
+    
+    // Replace spaces with hyphens
+    slug = slug.replace(/\s+/g, '-');
+    
+    // Replace multiple hyphens with single hyphen
+    slug = slug.replace(/-+/g, '-');
+    
+    // Remove leading/trailing hyphens
+    slug = slug.replace(/^-+|-+$/g, '');
+    
+    return slug;
+}
+
 $(document).ready(function() {
     // Initialize CKEditor for all textareas
     var editors = {};
@@ -431,11 +499,7 @@ $(document).ready(function() {
         var slugInput = $('.slug-input[data-lang="' + lang + '"]');
         
         if (slugInput.prop('readonly')) {
-            var slug = title.toLowerCase()
-                .replace(/[^a-z0-9\s-]/g, '')
-                .replace(/\s+/g, '-')
-                .replace(/-+/g, '-')
-                .trim('-');
+            var slug = generateSlugFinal(title, lang);
             slugInput.val(slug);
         }
     });
